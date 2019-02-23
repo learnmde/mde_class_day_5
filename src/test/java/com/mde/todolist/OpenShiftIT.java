dc3b351b409d9994b77378aef1130f0f83b42fcb
LearnMDE <learnmde@gmail.com>
2019-02-23 09:47:44 -0500

    Tested Dockerfile

--- src/test/java/com/mde/todolist/OpenShiftIT.java
+++ src/test/java/com/mde/todolist/OpenShiftIT.java
@@ -16,5 +16,62 @@
 
 package com.mde.todolist;
 
+import java.io.File;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
+import io.openshift.booster.test.OpenShiftTestAssistant;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static com.jayway.awaitility.Awaitility.await;
+import static com.jayway.restassured.RestAssured.get;
+import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.isEmptyString;
+
 public class OpenShiftIT {
+
+    private static OpenShiftTestAssistant assistant = new OpenShiftTestAssistant();
+
+    @BeforeClass
+    public static void prepare() throws Exception {
+        // Deploy the database and wait until it's ready.
+        assistant.deploy("database", new File("src/test/resources/templates/database.yml"));
+        assistant.awaitPodReadinessOrFail(
+                pod -> "my-database".equals(pod.getMetadata()
+                        .getLabels()
+                        .get("app"))
+        );
+        System.out.println("Database ready");
+
+        assistant.deployApplication();
+        assistant.awaitApplicationReadinessOrFail();
+
+        await().atMost(5, TimeUnit.MINUTES)
+                .until(() -> {
+                    try {
+                        Response response = get();
+                        return response.getStatusCode() < 500;
+                    } catch (Exception e) {
+                        return false;
+                    }
+                });
+
+        RestAssured.baseURI = RestAssured.baseURI + "/";
+    }
+
+    @AfterClass
+    public static void cleanup() {
+        assistant.cleanup();
+    }
+
+
+
 }
